51b635d3e875dc64fbe030a1493717e2d84200b7
JM Rohwer <j.m.rohwer@gmail.com>
2018-02-20 15:22:59 +0200

    docker command does not require sudo privileges

--- packaging/many_linux/run_pysces.sh
+++ packaging/many_linux/run_pysces.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+DNS_SERVER=8.8.4.4
+
 export DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
-sudo docker run --rm -v `pwd`:/io $DOCKER_IMAGE /io/build_pysces.sh
+docker run --dns $DNS_SERVER --rm -v `pwd`:/io $DOCKER_IMAGE /io/build_pysces.sh
 export DOCKER_IMAGE=quay.io/pypa/manylinux1_i686
-sudo docker run --rm -v `pwd`:/io $DOCKER_IMAGE /io/build_pysces.sh
+docker run --dns $DNS_SERVER --rm -v `pwd`:/io $DOCKER_IMAGE /io/build_pysces.sh
