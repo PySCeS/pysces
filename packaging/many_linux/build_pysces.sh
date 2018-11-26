c9850675a64cd8058b80adc4cfd35b49cb4c82c4
JM Rohwer <j.m.rohwer@gmail.com>
2018-11-26 20:32:26 +0200

    remove configparser dependency from build manylinux build script

--- packaging/many_linux/build_pysces.sh
+++ packaging/many_linux/build_pysces.sh
@@ -2,9 +2,6 @@
 
 for PYBIN in /opt/python/cp[23][67]*/bin; do
     ${PYBIN}/pip --cache-dir /io/packaging/many_linux/pip install numpy
-    if [ -n "$(echo ${PYBIN} | grep cp27)" ]; then
-        ${PYBIN}/pip --cache-dir /io/packaging/many_linux/pip install configparser
-    fi
     ${PYBIN}/python /io/setup.py bdist_wheel
 done
 
