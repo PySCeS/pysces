a0451b91f18015943eb69fa8ba5a9af87264c948
JM Rohwer <j.m.rohwer@gmail.com>
2021-08-26 11:31:05 +0200

    update conda build files

--- packaging/conda/build_conda_linux.sh
+++ packaging/conda/build_conda_linux.sh
@@ -2,9 +2,9 @@
 
 cp meta.linux.yaml meta.yaml
 
-# building Anaconda packages for pysces under Python 3.6, 3.7 and 3.8
+# building Anaconda packages for pysces under Python 3.6 - 3.9
 
 conda build --python 3.6 --numpy 1.15 .
 conda build --python 3.7 --numpy 1.15 .
 conda build --python 3.8 --numpy 1.17 .
-conda build --python 3.9 --numpy 1.19 .
+conda build --python 3.9 --numpy 1.20 .
--- packaging/conda/build_conda_windows.bat
+++ packaging/conda/build_conda_windows.bat
@@ -6,7 +6,7 @@ copy meta.windows.yaml meta.yaml
 call conda build --python 3.6 --numpy 1.15 .
 call conda build --python 3.7 --numpy 1.15 .
 call conda build --python 3.8 --numpy 1.17 .
-call conda build --python 3.9 --numpy 1.19 .
+call conda build --python 3.9 --numpy 1.20 .
 
 :: building wheels for Python 3.6, 3.7 and 3.8
 :: assumes appropriate conda environments are available (py3x-build)
@@ -15,7 +15,7 @@ call conda build --python 3.9 --numpy 1.19 .
 :: (base) C:\Users\user> conda create -n py36-build python=3.6 numpy=1.15
 :: (base) C:\Users\user> conda create -n py37-build python=3.7 numpy=1.15
 :: (base) C:\Users\user> conda create -n py38-build python=3.8 numpy=1.17
-:: (base) C:\Users\user> conda create -n py39-build python=3.9 numpy=1.19
+:: (base) C:\Users\user> conda create -n py39-build python=3.9 numpy=1.20
 cd ..\..
 
 call conda activate py36-build
